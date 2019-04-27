2a94fb8837e8beb19a9b039cd9992cd71e552345
psmaxwell <824810504@qq.com>
2019-04-27 09:34:05 +0800

    CRIDOOO1 Instance 108 Use loop/while sentence to get summary for 1-100

---
+++ 100.sql
@@ -0,0 +1,15 @@
+ /*使用loop循环求前100 个自然数 的和*/
+  
+ set serveroutput on
+ 
+ declare
+   sum_i int:= 0;
+   i int:= 0;
+ begin
+   loop
+     i:=i+1;
+     sum_i:=sum_i+i;
+     exit when i = 100;
+   end loop;
+   dbms_output.put_line('前100个自然数的和是: ' || sum_i);
+ end;
